2b2f190baa0899c4e24dc6adaaf10a81e51003ea
arthit.s <arthit.sekmoh@gmail.com>
2023-12-06 16:42:26 +0700

    Add files via upload

---
+++ GMMA_V2_class/GMMA_V2_class.ino
@@ -0,0 +1,272 @@
+#include "config.h"
+#include "ModbusRtu.h"
+#include <vector>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+Modbus slave(1, Serial1, 0);
+WiFiClient espClient;
+PubSubClient client(espClient);
+String status;
+String prv_status;
+String alarm_;
+String prv_alarm;
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // client.subscribe(topic_sub_1);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+
+  std::vector<std::vector<String>> def_tb;
+  // std::vector<std::vector<String>> data_tb;
+  std::vector<std::vector<String>> alarm_tb;
+
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1884);
+  client.setCallback(callback);
+  // Setup freertos
+  xTaskCreatePinnedToCore(modbus_Task, "Task1", 8000, NULL, 1, &Read_modbus, 0);
+  xTaskCreatePinnedToCore(func1_Task, "Task2", 10000, NULL, 1, &Func_1, 0);
+  xTaskCreatePinnedToCore(func2_Task, "Task3", 10000, NULL, 1, &Func_2, 0);
+  xTaskCreatePinnedToCore(func3_Task, "Task4", 10000, NULL, 1, &Func_3, 0);
+
+  Serial1.begin(115200);  // modbus port
+  Serial.println(esp_get_free_heap_size());
+  delay(1000);
+
+  slave.start();  // start modbus
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+}
+
+void modbus_Task(void *pvParam) {  // cytime = 0.2-1 ms
+  while (1) {
+    slave.poll(got_data, num_got_data);
+    //This loop ct = 0.5 ms
+    //record raw data to table
+    for (int i = 0; i < sizeof(def_tb) / sizeof(def_tb[0]); i++) {
+      if (def_tb[i][2] == "0" || def_tb[i][2] == "3") {
+        def_tb[i][3] = got_data[(def_tb[i][1].toInt())];
+      }
+    }
+    // clean data yy-->yyyy
+    for (int i = 0; i < sizeof(def_tb) / sizeof(def_tb[0]); i++) {
+
+      if (def_tb[i][0] == "yyyy" and def_tb[i][3].length() == 2) {  //select only yy
+        def_tb[i][3] = "20" + def_tb[i][3];
+      }
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+void func1_Task(void *pvParam) {  // type data -->1
+  while (1) {
+    bool change_1 = false;
+    StaticJsonDocument<300> json_1;  // size = 30*topic [avg]
+
+    // check data change
+    for (int i = 0; i < sizeof(def_tb) / sizeof(def_tb[0]); i++) {
+      if (def_tb[i][2] == "3") {
+        if (def_tb[i][3] != def_tb[i][4]) {
+          change_1 = true;
+          break;
+        }
+      }
+    }
+
+    if (change_1 == true) {  // data change !!!
+      for (int j = 0; j < sizeof(def_tb) / sizeof(def_tb[0]); j++) {
+        if (def_tb[j][2] == "3") {
+          json_1[String(def_tb[j][0])] = def_tb[j][3].toFloat();
+        }
+      }
+
+      json_1["rssi"] = (float)WiFi.RSSI();
+      String json_topic1;
+      serializeJson(json_1, json_topic1);
+      client.publish(topic_pub_1, json_topic1.c_str());
+
+      for (int k = 0; k < sizeof(def_tb) / sizeof(def_tb[0]); k++) {
+        if (def_tb[k][2] == "3") {
+          def_tb[k][4] = def_tb[k][3];
+        }
+      }
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(2000));
+  }
+}
+
+void func2_Task(void *pvParam) {  // status
+  while (1) {
+    bool ready_state= false;
+    bool check_data[2] = { false, false };
+    double bit_data[2];
+    uint8_t coil_no = 0;
+    //Convert & check data
+    for (int i = 0; i < 2; i++) {
+      if (got_data[i] >= 0) {
+        bit_data[i] = log2(got_data[i]);
+        if (bit_data[i] == round(bit_data[i])) {
+          check_data[i] = true;
+        } else {
+          check_data[i] = false;
+        }
+      }
+    }
+
+    if (bit_data[0] >= 0 and bit_data[1] >= 0) {
+      check_data[0] = false;
+      check_data[1] = false;
+    }
+    //get coil_no
+    for(int i=0;i<2;i++){
+      if(bit_data[i]>=0 and check_data[i] ==true){
+        coil_no = (i*16) + bit_data[i] ;
+        ready_state = true;
+      }
+    }
+
+    if (coil_no > 0 and ready_state == true) {
+      StaticJsonDocument<300> json_2;  // size = 30*topic [avg]
+      for (int i = 0; i < sizeof(def_tb) / sizeof(def_tb[0]); i++) {
+        if (def_tb[i][1].toInt() == coil_no) {
+          status = def_tb[i][0].toInt();
+          json_2["mc_status"] = status.toInt();
+        }
+      }
+      // Get datetime
+      for (int j = 0; j < sizeof(def_tb) / sizeof(def_tb[0]); j++) {
+        if (def_tb[j][2] == "0") {                              // 0--> d_time
+          json_2[String(def_tb[j][0])] = def_tb[j][3].toInt();  //yyyy-MM-dd HH:mm:ss
+        }
+      }
+      // publish data
+      if (status != prv_status) {
+        String json_topic2;
+        serializeJson(json_2, json_topic2);
+        client.publish(topic_pub_2, json_topic2.c_str());
+        ready_state = false;
+        prv_status = status;
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+void func3_Task(void *pvParam) {  // alarm
+  while (1) {
+    bool ready_state= false;
+    bool check_data[3] = { false, false,false };
+    double bit_data[3];
+    uint8_t coil_no = 0;
+    //Convert & check data
+    for (int i = 2; i < 5; i++) {
+      if (got_data[i] >= 0) {
+        bit_data[i] = log2(got_data[i]);
+        if (bit_data[i] == round(bit_data[i])) {
+          check_data[i] = true;
+        } else {
+          check_data[i] = false;
+        }
+      }
+    }
+
+    if (bit_data[0] >= 0 and bit_data[1] >= 0 and bit_data[2] >= 0) {
+      check_data[0] = false;
+      check_data[1] = false;
+      check_data[3] = false;
+    }
+    //get coil_no
+    for(int i=2;i<5;i++){
+      if(bit_data[i]>=0 and check_data[i] ==true){
+        coil_no = (i*16) + bit_data[i] ;
+        ready_state = true;
+      }
+    }
+
+    if (coil_no > 0 and ready_state == true) {
+      StaticJsonDocument<300> json_3;  // size = 30*topic [avg]
+      for (int i = 0; i < sizeof(def_tb) / sizeof(def_tb[0]); i++) {
+        if (def_tb[i][1].toInt() == coil_no) {
+          alarm_ = def_tb[i][0].toInt();
+          json_3["alarm_status"] = alarm_.toInt();
+        }
+      }
+      // Get datetime
+      for (int j = 0; j < sizeof(def_tb) / sizeof(def_tb[0]); j++) {
+        if (def_tb[j][2] == "0") {                              // 0--> d_time
+          json_3[String(def_tb[j][0])] = def_tb[j][3].toInt();  //yyyy-MM-dd HH:mm:ss
+        }
+      }
+      // publish data
+      if (alarm_ != prv_alarm) {
+        String json_topic3;
+        serializeJson(json_3, json_topic3);
+        client.publish(topic_pub_3, json_topic3.c_str());
+        ready_state = false;
+        prv_alarm = alarm_;
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
---
+++ GMMA_V2_class/config.h
@@ -0,0 +1,83 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// #### WIFI Config  #### //
+const char* ssid = "MIC_IIOT_2.4GHz";  //MIC_IIOT_2.4GHz MIC_IIOT Pwty_2.4G
+const char* password = "mic@admin";
+
+// #### MQTT Config  #### //
+
+const char* mqtt_server = "192.168.1.5"; 
+const char* topic_pub_1 = "simple_1";
+const char* topic_pub_2 = "simple_2";
+const char* topic_pub_3 = "simple_3";
+const char* topic_sub_1 = "sub_1";
+
+// #### Modbus Address Config  #### //
+const uint8_t itr_modbus = 100;  // ms
+const uint8_t itr_fnc_1 = 5000;  // ms
+const uint8_t itr_fnc_2 = 2000;  // ms
+const uint8_t itr_fnc_3 = 2000;  // ms
+const uint8_t num_got_data = 100;
+uint16_t got_data[num_got_data];
+
+String alarm_tb[][4]{
+  { "1", "32", "", "" },   //alarm1
+  { "2", "33", "", "" },   //alarm2
+  { "3", "34", "", "" },   //alarm3
+  { "4", "35", "", "" },   //alarm4
+  { "5", "36", "", "" },   //alarm5
+  { "6", "37", "", "" },   //alarm6
+  { "7", "38", "", "" },   //alarm7
+  { "8", "39", "", "" },   //alarm8
+  { "9", "40", "", "" },   //alarm9
+  { "40", "71", "", "" },  //alarm40
+};
+String def_tb[][5] = {
+  // name||address||type||value||prv_value
+  // type for separate detail of data
+  { "1", "5", "1", "", "" },    //run
+  { "2", "6", "1", "", "" },    //stop
+  { "3", "7", "1", "", "" },    //alarm
+  { "4", "8", "1", "", "" },    //adjust
+  { "5", "9", "1", "", "" },    //setup
+  { "6", "10", "1", "", "" },    //ch_tool
+  { "7", "11", "1", "", "" },    //warm
+  { "8", "12", "1", "", "" },    //other
+  { "20", "24", "1", "", "" },  //last status
+  { "1", "32", "2", "", "" },   //alarm1
+  { "2", "33", "2", "", "" },   //alarm2
+  { "3", "34", "2", "", "" },   //alarm3
+  { "4", "35", "2", "", "" },   //alarm4
+  { "5", "36", "2", "", "" },   //alarm5
+  { "6", "37", "2", "", "" },   //alarm6
+  { "7", "38", "2", "", "" },   //alarm7
+  { "8", "39", "2", "", "" },   //alarm8
+  { "9", "40", "2", "", "" },   //alarm9
+  { "40", "71", "2", "", "" },  //alarm40
+  { "yyyy", "80", "0", "", "" },
+  { "mm", "81", "0", "", "" },
+  { "dd", "82", "0", "", "" },
+  { "hh", "83", "0", "", "" },
+  { "min", "84", "0", "", "" },
+  { "sec", "85", "0", "", "" },
+  { "ttl_a", "90", "3", "", "" },
+  { "ttl_b", "91", "3", "", "" },
+  { "ttl_c", "92", "3", "", "" },
+  { "yeild_a", "93", "3", "", "" },
+  { "yeild_b", "94", "3", "", "" },
+  { "yeild_c", "95", "3", "", "" },
+  { "tray_1", "96", "3", "", "" },
+  { "tray_2", "97", "3", "", "" },
+  { "tray_3", "98", "3", "", "" },
+  { "tray_main", "99", "3", "", "" }
+};
+
+
+// #### Freertos  Config  #### //
+TaskHandle_t Read_modbus = NULL;
+TaskHandle_t Func_1 = NULL;  //data
+TaskHandle_t Func_2 = NULL;  // status
+TaskHandle_t Func_3 = NULL;  // alarm
+
+#endif
