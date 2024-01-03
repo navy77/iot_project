fe69425b044e225c3b1e25ae68ea776c4c89c75e
arthit.s <arthit.sekmoh@gmail.com>
2024-01-03 23:28:48 +0700

    Update MicMMS.h

--- mms_v1/MicMMS.h
+++ mms_v1/MicMMS.h
@@ -14,18 +14,23 @@ private:
   const char* password;
   const char* mqtt_server;
   int mqtt_port;
+  const char* mc_no;
   WiFiClient wifiClient;
   PubSubClient mqttClient;
 
   int slaveId;
   HardwareSerial& serialPort;
   Modbus modbus;
-  
+
+  IPAddress ip;
+  IPAddress gateway;
+  IPAddress subnet;
 public:
-  MicMMS(const char* ssid, const char* password, const char* mqtt_server, int mqtt_port,const char* mc_no, int slaveId, HardwareSerial& serialPort);
+  MicMMS(const char* ssid, const char* password, const char* mqtt_server, int mqtt_port, const char* mc_no, int slaveId, HardwareSerial& serialPort,const char* ip_address, const char* gateway_address, const char* subnet_mask);
+  
   void init();
   void reconnect();
-  void publishMessage(const char* topic, const char* message);
+  void publishMessage(char* topic, const char* message);
   void run();
   void start();
   static void modbus_Task(void* pvParam);
@@ -33,7 +38,6 @@ public:
   static void func2_Task(void* pvParam);
   static void func3_Task(void* pvParam);
   static void esp_Task(void* pvParam);
-
 };
 
 #endif
