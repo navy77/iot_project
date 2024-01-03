910a624b00147f465ac82ab04eb7d8278163daba
arthit.s <arthit.sekmoh@gmail.com>
2024-01-03 23:32:23 +0700

    Update mms_v1.ino

--- mms_v1/mms_v1.ino
+++ mms_v1/mms_v1.ino
@@ -1,9 +1,11 @@
 #include "MicMMS.h"
-MicMMS aaa("MIC_Iot", "Micdev@2024", "192.168.0.160", 1884, 1, Serial1);
 
+MicMMS aaa("Pwty_2.4G", "1234abcd", "192.168.1.69", 1884,"A02", 1, Serial1,"192.168.1.100","192.168.1.1","255.255.255.0");
+// xxx(ssid,password,mqttBroker,mqttPort,slaveID,modbusPort,FixIP,gateway,subnet);
 void setup() {
   aaa.init();
   aaa.start();
+
 }
   
 void loop() {
